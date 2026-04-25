f24368838291c63b71fb8e5f5a4ca8f4b4c8778f
PAVAN KALYAN CHAKRAVARTHY POSA <97937105+Pavan-posa@users.noreply.github.com>
2026-04-25 19:20:02 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,8 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
   vite: {
-    // optional overrides go here
     build: {
-      outDir: "dist", // make sure Vercel sees this
+      outDir: "dist",   // Vercel expects this folder
     },
   },
 });
